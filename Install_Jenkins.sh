73f4eff1705b780ac9f1fe7de1dcd5cadecea693
GuillermoITV <31926950+GuillermoITV@users.noreply.github.com>
2021-08-06 14:01:57 -0500

    Update Install_Jenkins.sh

--- Install_Jenkins.sh
+++ Install_Jenkins.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 yum install java-1.8.0-openjdk-devel -y
 yum install wget -y
-wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat-stable/jenkins.repo
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat-stable/jenkin                                                                             s.repo
 rpm --import http://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install jenkins -y
 systemctl start jenkins
 systemctl status jenkins
-sleep 20s
+sleep 10s
 cat /var/lib/jenkins/secrets/initialAdminPassword
+yum install maven -y
+yum install unzip -y
+wget https://chromedriver.storage.googleapis.com/92.0.4515.107/chromedriver_linu                                                                             x64.zip
+unzip chromedriver_linux64.zip
+mv chromedriver /usr/bin/chromedriver
+chromedriver –version
+curl https://intoli.com/install-google-chrome.sh | bash
+sudo curl https://intoli.com/install-google-chrome.sh
