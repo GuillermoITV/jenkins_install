e1e97ce81f706eb1ed3b891b723404be876ec0bf
GuillermoITV <31926950+GuillermoITV@users.noreply.github.com>
2021-10-04 12:43:03 -0500

    Update Install_Jenkins.sh

--- Install_Jenkins.sh
+++ Install_Jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum install java-1.8.0-openjdk-devel -y
 yum install wget -y
-wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat-stable/jenkins.repo
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat-stable/jenkins.repo --no-check-certificate
 rpm --import http://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install jenkins -y
 systemctl start jenkins
